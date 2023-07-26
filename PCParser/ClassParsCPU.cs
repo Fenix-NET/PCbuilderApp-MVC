e7957ba78bd90e5f3e2348d9d8479cfca44303fa
Fenix <gromazekazeka@gmail.com>
2023-07-26 23:23:46 +0300

    Отправная точка парсера

---
+++ PCParser/ClassParsCPU.cs
@@ -0,0 +1,99 @@
+﻿using AngleSharp.Html.Dom;
+using AngleSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp.Dom;
+using System.Text.RegularExpressions;
+
+namespace PCParser
+{
+    public class ClassParsCPU
+    {
+
+
+
+        public async void StartParsCPU()
+        {
+
+            Console.WriteLine("Hello, World!");
+
+            var config = Configuration.Default.WithDefaultLoader();
+            var address = "https://tula.nix.ru/price.html?section=cpu_all#c_id=161&fn=161&g_id=7&page=1&sort=%2Bp8175%2B1605%2B7287%2B766%2B2326&spoiler=&store=region-1483_0&thumbnail_view=2";
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(address);
+            var urlSelector = "a.t"; //html element to get book names
+            var cells = document.QuerySelectorAll(urlSelector).OfType<IHtmlAnchorElement>();
+            var titlesRef = cells.Select(m => m.Href).ToList();
+            var priceSelector = "td.d.tac.cell-price > span"; //html element to get book names
+            var cellsP = document.QuerySelectorAll(priceSelector);
+
+
+            var titlesPrice = cellsP.Select(m => m.TextContent.Replace(" ", "")).ToList();
+
+            List<CPUparse> CPUs = new List<CPUparse>();
+            
+            Console.WriteLine("Начало парсинга CPU");
+            var manufacturerSelector = "td#tdsa2943";
+            var modelSelector = "td#tdsa2944";                 //"td#tdsa2944 <a.btn btn-i btn-t-1 btn-c-1 btn-c-2-b"
+            var socketSelector = "td#tdsa1307";
+            
+            IElement cellss;
+            int x = 0;
+            for (int i = 0; i < titlesRef.Count; i++)
+            {
+                CPUs.Add(new CPUparse());
+                CPUs[x].Price = decimal.Parse(titlesPrice[i]);
+                address = titlesRef[i];
+                document = await context.OpenAsync(address);
+
+                //var manufacturerSelector = "td#tdsa2943";    //"td#tdsa2943"a.add_to_cart.btn.btn-t-0.btn-c-6.CanBeSold.pc-component"
+                cellss = document.QuerySelector(manufacturerSelector);
+                CPUs[x].Manufacturer = cellss?.TextContent;
+
+                // var modelSelector = "td#tdsa2944";
+                cellss = document.QuerySelector(modelSelector);
+                CPUs[x].Model = cellss.FirstChild.TextContent;
+
+                // var powerSelector = "td#tdsa44456";
+                cellss = document.QuerySelector(socketSelector);
+
+                CPUs[x].Socket = cellss?.FirstChild.TextContent;
+
+                x++;
+            }
+            Console.WriteLine("Конец работы");
+
+            for (int i = 0; i < CPUs.Count; i++)
+            {
+                Console.WriteLine($"Производитель : {CPUs[i].Manufacturer}");
+                Console.WriteLine($"Модель : {CPUs[i].Model}");
+                Console.WriteLine($"Цена : {CPUs[i].Price}");
+                Console.WriteLine($"Сокет : {CPUs[i].Socket}");
+                Console.WriteLine("================================================================");
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,21 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AngleSharp;
-using AngleSharp.Dom;
 using AngleSharp.Html.Dom;
-using PCParser;
-using System.ComponentModel;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Linq;
-using System;
-using System.Threading.Tasks;
-using System.Collections.Generic;
+using Test;
 
 Console.WriteLine("Hello, World!");
 
 
 var config = Configuration.Default.WithDefaultLoader();
-var address = "https://tula.nix.ru/price.html?section=video_cards_all#c_id=101&fn=101&g_id=685&page=1&sort=%2Bp965%2B214%2B215&spoiler=&store=region-1483_0&thumbnail_view=2";
+var address = "https://tula.nix.ru/price.html?section=video_cards_all#c_id=101&fn=101&g_id=524&page=1&sort=%2Bp965%2B214%2B215&spoiler=&store=region-1483_0&thumbnail_view=2";
 var context = BrowsingContext.New(config);
 var document = await context.OpenAsync(address);
 var urlSelector = "a.t"; //html element to get book names
@@ -25,18 +18,12 @@ var priceSelector = "td.d.tac.cell-price > span"; //html element to get book nam
 var cellsP = document.QuerySelectorAll(priceSelector);
 
 
-var titlesPrice = cellsP.Select(m => m.TextContent.Replace(" ", "")).ToList();
+var titlesPrice = cellsP.Select(m => m.TextContent.Replace(" ", ""));
 
 List<GPUparse> GPUs = new List<GPUparse>();
 
 int x = 0;
 Console.WriteLine("Начало парсинга GPU");
-
-var manufacturerSelector = "td#tdsa2943";
-var modelSelector = "td#tdsa2944";                 //"td#tdsa2944 <a.btn btn-i btn-t-1 btn-c-1 btn-c-2-b"
-var powerSelector = "td#tdsa44456";
-var powerSelectorNull = "td#tdsa893";
-IElement cellss;
 for (int i = 0; i < titlesRef.Count; i++)
 {
     GPUs.Add(new GPUparse());
@@ -44,18 +31,18 @@ for (int i = 0; i < titlesRef.Count; i++)
     address = titlesRef[i];
     document = await context.OpenAsync(address);
 
-    //var manufacturerSelector = "td#tdsa2943";    //"td#tdsa2943"a.add_to_cart.btn.btn-t-0.btn-c-6.CanBeSold.pc-component"
-    cellss = document.QuerySelector(manufacturerSelector);
+    var manufacturerSelector = "td#tdsa2943";    //"td#tdsa2943"a.add_to_cart.btn.btn-t-0.btn-c-6.CanBeSold.pc-component"
+    var cellss = document.QuerySelector(manufacturerSelector);
     GPUs[x].Manufacturer = cellss?.TextContent;
 
-   // var modelSelector = "td#tdsa2944";
+    var modelSelector = "td#tdsa2944";
     cellss = document.QuerySelector(modelSelector);
-    GPUs[x].Model = cellss.FirstChild.TextContent;
+    GPUs[x].Model = cellss?.TextContent;
 
-   // var powerSelector = "td#tdsa44456";
-    cellss = document.QuerySelector(powerSelector) ?? document.QuerySelector(powerSelectorNull);
-
-    GPUs[x].Power = ushort.Parse(Regex.Replace(cellss?.TextContent, @"\D+", ""));
+    var powerSelector = "td#tdsa44456";
+    cellss = document.QuerySelector(powerSelector);
+    string? power = Regex.Replace(cellss?.TextContent, @"\D+", "");
+    GPUs[x].Power = ushort.Parse(power);
 
     x++;
 }
@@ -69,8 +56,6 @@ for (int i = 0; i < GPUs.Count; i++)
     Console.WriteLine("================================================================");
 }
 
-ClassParsCPU classPars = new ClassParsCPU();
-classPars.StartParsCPU();
 //Console.WriteLine("Начало парсинга CPU");
 //address = "https://www.xcom-shop.ru/catalog/collections/cpu_for_games/";  //url каталога CPU
 //document = await context.OpenAsync(address);
